dfe386c0bb0293b29773bbe55cce01068b7866ae
Support <Support@DESKTOP-UT7PTJ0>
2024-05-11 04:45:57 -0700

    Add project files.

---
+++ Services/ProductServices/Services/Dto/ProductUrlResolver.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using AutoMapper.Execution;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+using Services.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ProductServices.Services.Dto
+{
+
+    public class ProductUrlResolver : IValueResolver<Product, ProductResultDto, string>
+    {
+        private readonly IConfiguration _Configuration;
+
+        public ProductUrlResolver(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        public string Resolve(Product source, ProductResultDto destination, string destMember, ResolutionContext context)
+        {
+            if(!string.IsNullOrEmpty(source.PictureUrl))
+                return _Configuration["BaseUrl"] + source.PictureUrl;
+            
+            return null;
+        }
+    }
+}
